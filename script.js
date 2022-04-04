8717e81edce6eb3be234db6c63eacc812253662c
Katy Brown <katybr35@gmail.com>
2022-04-04 11:18:49 -0500

    date is working. Trying to get 5 day forecast to work

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ var currentWindEl = $(".currentWind");
 var currentHumidityEl = $(".currentHumidity");
 var uvIndexEl = $(".uvIndex");
 
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
 searchBtnEl.on("click", function (event) {
   console.log(userInput[0].value);
   var locationApiUrl =
@@ -23,8 +26,7 @@ searchBtnEl.on("click", function (event) {
       var name = data[0].name;
       console.log(lat, lon, name);
 
-     document.getElementById("Name").textContent = "Name:" + " " + name
-     
+      document.getElementById("Name").textContent = "Name:" + " " + name;
 
       function fetchWeather(lat, lon) {
         var weatherApiUrl =
@@ -32,7 +34,7 @@ searchBtnEl.on("click", function (event) {
           lat +
           "&lon=" +
           lon +
-          "&appid=43261659cb8fa178b54f17f76141e0a4";
+          "&appid=43261659cb8fa178b54f17f76141e0a4&units=imperial&cnt=5";
 
         console.log(weatherApiUrl);
 
@@ -44,25 +46,47 @@ searchBtnEl.on("click", function (event) {
             var currentWindValue = data.current.wind_speed;
             var currentHumidityValue = data.current.humidity;
             var currentUvIndexValue = data.current.uvi;
-            var descValue = data.current.weather[0].description;
+            //var descValue = data.current.weather[0].description;
 
-
-            document.getElementById("Temp").textContent = "Temp:" + " " + tempValue
-            document.getElementById("Icon").textContent = weatherIconValue
-            document.getElementById("Wind").textContent = "Wind:" + " " + currentWindValue
-            document.getElementById("Humidity").textContent = "Humidity:"+ " " + currentHumidityValue
-            document.getElementById("Index").textContent = "UV Index" + " " + currentUvIndexValue
+            document.getElementById("Temp").textContent =
+              "Temp:" + " " + tempValue + "\u00B0 F";
+            document.getElementById("Icon").textContent = weatherIconValue;
+            document.getElementById("Wind").textContent =
+              "Wind:" + " " + currentWindValue + "MPH";
+            document.getElementById("Humidity").textContent =
+              "Humidity:" + " " + currentHumidityValue + "%";
+            document.getElementById("Index").textContent =
+              "UV Index" + " " + currentUvIndexValue;
 
             console.log(data);
-            console.log(tempValue);
-            console.log(weatherIconValue);
-            console.log(currentWindValue);
-            console.log(currentHumidityValue);
-            console.log(currentUvIndexValue);
-            console.log(descValue);
+            // console.log(tempValue);
+            // console.log(weatherIconValue);
+            // console.log(currentWindValue);
+            // console.log(currentHumidityValue);
+            // console.log(currentUvIndexValue);
+            // console.log(descValue);
+
+            // function fetchForcast(lat, lon) {
+            //   var forecastAPI =
+            //   "https://api.openweathermap.org/data/2.5/onecall?lat="+
+            //     lat +
+            //     "&lon=" +
+            //     lon +
+            //     "&appid=0e0302d8420bd377d72e92eb9e2f4787&units=imperial&cnt=5";
+
+            //   //console.log(forcastAPI);
+            //   fetch(forecastAPI)
+            //   .then((response) => response.json())
+            //   .then((data) => {
+            //     console.log(data);
+            //   });
+            // }
+            // fetchForcast(lat, lon);
+              
+           
           });
       }
 
       fetchWeather(lat, lon);
-      });
-});
+    });
+  });
